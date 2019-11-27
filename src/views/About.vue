2ebbfac635b589038283c81e151934dc0a20acb6
Alejandro Barros <abarros@waypoint.cl>
2019-11-26 22:35:29 -0800

    add page about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="img-banner" :style="{'background-image': 'url(' + require('../assets/petLogo2.jpeg') + ')'}">
-    <h1>This is an about page</h1>
+  <div class="card mb-3">
+  <img src="@/assets/petlogo.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Pet Store</h5>
+    <p class="card-text">App developed with framework vuejs by alejbarros.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
   </div>
+</div>
 </template>
+<style scoped lang="scss">
+
+</style>
